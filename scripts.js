566933ef6aeb2a8e57e68488dfa5b7eacff7a085
jackmacaa <jackmaca@msn.com>
2025-01-14 20:56:53 +1030

    added js script for years old age calc

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const getBirthYears = (dob) => {
+  const birthday = new Date(dob);
+
+  const ageDifMs = Date.now() - birthday.getTime();
+
+  const ageDate = new Date(ageDifMs);
+
+  return Math.abs(ageDate.getUTCFullYear() - 1970) + " Years Old";
+};
+
+const yearsOld = getBirthYears("1991-04-12");
+
+const yearsOldElement = (document.getElementById("yearsOldId").innerHTML = yearsOld);
